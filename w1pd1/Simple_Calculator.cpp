a262776449f4f6122b03fa6bb098f83994cbb610
shufol islam <shufolislam1255@gmail.com>
2023-06-04 22:10:54 +0600

    started prac problem

---
+++ w1pd1/Simple_Calculator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int x,y;
+    cin>>x>>y;
+    printf("%lld + %lld = %lld\n", x,y,x+y);
+    printf("%lld * %lld = %lld\n", x,y,x*y);
+    printf("%lld - %lld = %lld\n", x,y,x-y);
+    return 0;
+}
---
+++ w1pd1/pp1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int get_array(int n){
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+}
+int main(){
+    int n;
+    get_array(n);
+    return 0;
+}
