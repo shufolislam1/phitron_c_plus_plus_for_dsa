b826e3d0d66276e000c41609e99b831c12a287cc
shufol islam <shufolislam1255@gmail.com>
2023-06-04 21:39:39 +0600

    all done

--- Final_Exam/one.cpp
+++ Final_Exam/one.cpp
@@ -1,42 +1,24 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int n;
-//     cin>>n;
-//     for(int i=0; i<n; i++){
-//         string s,x;
-//         cin>>s>>x;
-
-//     }
-//     return 0;
-// }
-
-#include <iostream>
-#include <string>
+#include<bits/stdc++.h>
 using namespace std;
-
-string replaceString(string str, string pattern, char replaceChar) {
-    size_t pos = 0;
-    while ((pos = str.find(pattern, pos)) != string::npos) {
-        str.replace(pos, pattern.length(), 1, replaceChar);
-        pos += 1; // Move past the replaced character
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        string s,x;
+        cin>>s>>x;
+        while(true){
+            int found = s.find(x);
+            // jodi pawa jai tahole if e dhukbe
+            if(found != -1){
+                s.replace(found, x.size(), "$");
+            }else{
+                break;   //na paile while loop  break kore dibe.
+            }
+        }
+        // if(s.find(x)){
+        //     s.replace();
+        // }
+        cout<<s<<endl;
     }
-    return str;
-}
-
-int main() {
-    int T;
-    cin >> T;
-    cin.ignore(); // Ignore the newline character after reading T
-
-    while (T--) {
-        string S, X;
-        getline(cin, S, ' ');
-        getline(cin, X);
-
-        string modifiedString = replaceString(S, X, '$');
-        cout << modifiedString << endl;
-    }
-
     return 0;
 }
