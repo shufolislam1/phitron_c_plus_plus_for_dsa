9d8d141ff7422e8c05d8bd124fd18b22b64ab206
shufol islam <shufolislam1255@gmail.com>
2023-06-04 11:02:20 +0600

    two done

--- Final_Exam/Get_Reverse_II.cpp
+++ Final_Exam/Get_Reverse_II.cpp
@@ -1,6 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+class Student
+{
+public:
+    string name;
+    int cls;
+    char sec;
+    int id;
+};
+int main()
+{
+    int n;
+    cin >> n;
+    int start = 0;
+    int end = n - 1;
+    cin.ignore();
+    Student obj[n];
+    for (int i = 0; i < n; i++)
+    {
+        // Student obj;
+        cin >> obj[i].name >> obj[i].cls >> obj[i].sec >> obj[i].id;
+    }
+    while (start < end)
+    {
 
+        int temp = obj[start].id;
+        obj[start].id = obj[end].id;
+        obj[end].id = temp;
+
+        start++;
+        end--;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << obj[i].name << " " << obj[i].cls << " " << obj[i].sec << " " << obj[i].id << endl;
+    }
     return 0;
 }
